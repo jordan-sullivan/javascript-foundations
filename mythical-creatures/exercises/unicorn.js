e883de658ad09eb3803521dddf374804c62d1cf8
Jordan Sullivan <helloandhi.jp@gmail.com>
2022-02-07 22:35:22 -0700

    Complete Unicorn test

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,8 +1,25 @@
 class Unicorn {
   constructor(name, color) {
     this.name = name;
-    this.color = color;
+    if (color === undefined) {
+      this.color = "white";
+    } else {
+      this.color = color;
+    }
   }
+  isWhite() {
+    if (this.color !== "white") {
+      return false;
+    }
+  }
+ says(saying) {
+   return `**;* ${saying} *;**`
+ }
 }
+
+//read the test you're running
+//ask yourself if it will fail and why
+//run the test
+//make it pass
 
 module.exports = Unicorn;
--- mythical-creatures/test/unicorn-test.js
+++ mythical-creatures/test/unicorn-test.js
@@ -15,38 +15,54 @@ describe('Unicorn', function() {
 
   it('should have a name', function() {
     var unicorn = new Unicorn('Bree');
-
+//unicorn is an instance of the class Unicorn
     assert.equal(unicorn.name, 'Bree');
+    //assert.equal(actual, expected to have value of Bree)
+//we know from this test that we need a name parameter,
+//so add a parameter, name
+//then we need to add this.name to the class
+//because it has a name property
+//we could have hard coded the string this.name = "Bree";
+//but we want to keep our code dynamic, so add in the parameter
+//of name instead in this instance
   });
 
   it('should be able to have a different name', function() {
     var unicorn = new Unicorn('Pam');
 
     assert.equal(unicorn.name, 'Pam');
+    //testing name property for unicorm instance
   });
 
   it('should have a color', function() {
     var unicorn = new Unicorn('TayTay', 'blue');
-    
+
     assert.equal(unicorn.color, 'blue');
   });
 
-  it.skip('should be white as default', function() {
+  it('should be white as default', function() {
     var unicorn = new Unicorn('Louisa');
 
     assert.equal(unicorn.color, 'white');
+    //testing a color property for unicorn instance
   });
 
-  it.skip('should refute isWhite', function() {
+  it('should refute isWhite', function() {
     var unicorn = new Unicorn('Steve', 'red');
 
     assert.equal(unicorn.isWhite(), false);
+    //we are actually invoking isWhite, a method
+    //the return value of false will need to be second argument
+    //we're calling it because of parentheses, so this tells us
+    //we need to create this function on other side
   });
 
-  it.skip('should say sparklying stuff', function() {
+  it('should say sparklying stuff', function() {
     var unicorn = new Unicorn('Brenna');
-    
+
     assert.equal(unicorn.says('Wonderful!'), '**;* Wonderful! *;**');
+//invoking says method and passing in one argument to method,
+//so our method will need a parameter
     assert.equal(unicorn.says('Batman has a great smile'), '**;* Batman has a great smile *;**');
   });
 });
